e97c2913ffbb249b7506e5b2df410fae73f7a8db
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-04-29 08:51:37 +0200

    refactor: :fire: Removed unnecesary any

--- src/decorators/HttpMethodDecorator.ts
+++ src/decorators/HttpMethodDecorator.ts
@@ -1,7 +1,7 @@
 import { HttpMethod } from "../enums";
 
 export function HttpMethodDecorator(method: HttpMethod, route: string) {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function (target: Function, propertyKey: string, descriptor: PropertyDescriptor) {
         Reflect.defineMetadata('route', route, target, propertyKey);
         Reflect.defineMetadata('method', method, target, propertyKey);
     };
