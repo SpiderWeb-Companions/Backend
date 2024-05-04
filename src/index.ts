d35106c7cc64c4278eff6fe9129aa9135021e155
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-04 13:26:33 +0200

    feat: :poop: CORS 🤮

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ import {
 const app = express();
 app.use(express.json());
 app.use(logRequest);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const port = 3000;
 // Register controllers here by adding controller class to array
 registerControllers(app, [
