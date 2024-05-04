3b212803efadc63f26960732679477afee154afb
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-04 09:38:01 +0200

    fix: :goal_net: Added generic way to catch errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { Application, Router, RequestHandler, IRouter } from 'express';
 import { controller } from './interfaces';
 import { HttpMethods } from './types';
+import { Logger } from "./logging/logger";
 
 function isHttpMethod(method: string): method is HttpMethods {
   return (
@@ -14,13 +15,25 @@ function isHttpMethod(method: string): method is HttpMethods {
   );
 }
 
+function errorWrapper(handler: RequestHandler): RequestHandler {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+      Logger.error(errorMessage);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+}
 export function registerControllers(app: Application, controllers: controller[]) {
   controllers.forEach((controller) => {
     const router = Router();
     for (const [key, value] of Object.entries(controller.endpoints)) {
       const method: string = value.method;
       if (isHttpMethod(method)) {
-        (router as IRouter)[method](key, value.handler as RequestHandler);
+        const wrappedHandler = errorWrapper(value.handler as RequestHandler);
+        (router as IRouter)[method](key, wrappedHandler);
       } else {
         console.warn(`Invalid HTTP method: ${method}`);
       }
