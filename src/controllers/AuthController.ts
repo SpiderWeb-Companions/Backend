61d7231d8e0d33543a78f1a100dddd49f56f4c22
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-05-07 18:27:09 +0200

    resolved comments

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -13,16 +13,16 @@ export class AuthController implements controller {
   // define these to make the array in index.ts happy
   static endpoint = '';
   static endpoints = {}
+  static clientId = process.env.CLIENT_ID;
+  static redirectUri = process.env.REDIRECT_URI;
+  static clientSecret = process.env.CLIENT_SECRET;
 
 
   @Post('/auth')
   async Authenticate(req: Request, res: Response<AuthResponse>) {
     const { code } = req.body; 
-    const clientId = process.env.CLIENT_ID;
-    const redirectUri = process.env.REDIRECT_URI;
-    const clientSecret = process.env.CLIENT_SECRET;
 
-    let tokenURL =`https://oauth2.googleapis.com/token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&grant_type=authorization_code&code=${code}`
+    let tokenURL =`https://oauth2.googleapis.com/token?client_id=${AuthController.clientId}&client_secret=${AuthController.clientSecret}&redirect_uri=${AuthController.redirectUri}&grant_type=authorization_code&code=${code}`
     let token : AuthResponse | undefined;
     try {
       token = await authenticateUser(tokenURL);
