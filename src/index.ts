bd3b5588be6fe51197e07ef5e6fa179a922d511f
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-08 20:49:44 +0200

    feat: :lock: cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(logRequest);
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
