983421baf1c0d059199158ddf7a93fdea7588c2b
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-05-09 17:06:16 +0200

    fix

--- src/MiddleWare/AuthMiddleware.ts
+++ src/MiddleWare/AuthMiddleware.ts
@@ -2,23 +2,28 @@ import { Request, Response, NextFunction } from 'express';
 import { Logger } from '../logging/logger';
 import { isAuthenticated } from "../util/isAuthenticated";
 
-const unauth = ['/api/hello', 'api/auth'];
-const allowedMethods = ['GET', 'POST', 'PUT'];
+const unauth = ['/api/hello', '/api/auth'];
+const allowedMethods = ['GET', 'POST', 'PUT',];
 
 export async function validateAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+  debugger;
   if(unauth.includes(req.url)) {
     Logger.info(`Unauthed route: ${req.url}`);
     next();
     return;
   }
+  if (req.method== 'OPTIONS'){
+    next();
+    return;
+  }
   if (allowedMethods.includes(req.method)) {
-    let token: string | string[] | undefined = req.headers['Authorization'];
+    let token: string | string[] | undefined = req.headers['authorization'];
     if (token && !Array.isArray(token)) {
       let isAuth: boolean = await isAuthenticated(token);
-      Logger.debug(`${req.method} ${req.url} ${req.headers['Authorization']} `);
+      Logger.debug(`${req.method} ${req.url} ${req.headers['authorization']} `);
       next();
     }
     return;
   }
-  Logger.debug(`failed: ${req.method} ${req.url} ${req.headers['Authorization']} `);
+  Logger.debug(`failed: ${req.method} ${req.url}  `);
 }
